2ea9038de7717d357c2d2a08cb9e4e78a241a359
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-03-13 15:44:03 +0200

    Set current user to null if any request returns unauthorized

--- lib/src/services/split_bill_api/interceptors/unauthorized_interceptor.dart
+++ lib/src/services/split_bill_api/interceptors/unauthorized_interceptor.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:splitbill_client/src/viewmodels/current_user_view_model.dart';
 
 InterceptorsWrapper unauthorizedInterceptor() {
+  final _userProvider = ProviderContainer().read(currentUserViewModel);
+
   return InterceptorsWrapper(
     onResponse: (Response response) {
+      if (response.statusCode == HttpStatus.unauthorized) {
+        _userProvider.user(null);
+      }
     },
   );
 }
