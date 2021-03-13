c3be97c697a6f839f53ec0580143721d2023cb6e
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-03-13 11:39:15 +0200

    Add router

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:splitbill_client/src/application.dart';
-import 'package:splitbill_client/src/screens/login/login_screen.dart';
 
 void main() {
   Application.init();
@@ -13,10 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      onGenerateRoute: Application.router.generator,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
     );
   }
 }
