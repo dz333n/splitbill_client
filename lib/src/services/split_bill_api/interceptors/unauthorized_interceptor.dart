3d856f793f771ba1daecce428eb6e135bd100b0c
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-04-10 11:55:30 +0300

    Update unathorizedIntercepter to handle errors and drop token if needed

--- lib/src/services/split_bill_api/interceptors/unauthorized_interceptor.dart
+++ lib/src/services/split_bill_api/interceptors/unauthorized_interceptor.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:splitbill_client/src/routes/login_route.dart';
+import 'package:splitbill_client/src/services/split_bill_api/token_manager.dart';
 import 'package:splitbill_client/src/viewmodels/current_user_view_model.dart';
 import 'package:splitbill_client/src/application.dart';
 
@@ -8,9 +10,12 @@ InterceptorsWrapper unauthorizedInterceptor() {
   final _userProvider = Application.container.read(currentUserViewModel);
 
   return InterceptorsWrapper(
-    onResponse: (Response response) {
-      if (response.statusCode == HttpStatus.unauthorized) {
-        _userProvider.user(null);
+    onError: (DioError error) async {
+      if (error.response.statusCode == HttpStatus.unauthorized) {
+        _userProvider.user = null;
+        await TokenManager.saveToken(null);
+        Application.navigator.currentState
+            .pushReplacementNamed(LoginRoute.constructPath());
       }
     },
   );
