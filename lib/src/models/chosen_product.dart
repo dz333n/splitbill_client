406023b8835c54fcf2df6a6329a03b78a0db442d
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-04-10 12:52:01 +0300

    Create ChosenProduct model

---
+++ lib/src/models/chosen_product.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:splitbill_client/src/models/product.dart';
+
+part 'chosen_product.g.dart';
+
+@JsonSerializable()
+class ChosenProduct {
+  final Product product;
+  final double coefficient;
+
+  ChosenProduct({ this.product, this.coefficient });
+
+  factory ChosenProduct.fromJson(Map<String, dynamic> json) => _$ChosenProductFromJson(json);
+  Map<String, dynamic> toJson() => _$ChosenProductToJson(this);
+}
