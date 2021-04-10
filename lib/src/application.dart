cf75303a66c4b4cf996ffb0f68dfcfe816b1f8d8
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-04-10 11:55:08 +0300

    Define global key for navigator

--- lib/src/application.dart
+++ lib/src/application.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:splitbill_client/src/application_routes.dart';
 
 class Application {
+  static final navigator = GlobalKey<NavigatorState>();
   static ProviderContainer container;
   static ApplicationRouter router;
   static FlutterSecureStorage storage;
