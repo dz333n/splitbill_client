ee5c9531a57db4abf36181c374d2861d43bd1179
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-04-10 16:01:26 +0300

    Implement TakePartButton

--- lib/src/screens/bill_details/components/chosen_product.dart
+++ lib/src/screens/bill_details/components/chosen_product.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:splitbill_client/src/models/chosen_product.dart';
 import 'package:splitbill_client/src/screens/bill_details/components/product_description.dart';
+import 'package:splitbill_client/src/screens/bill_details/components/take_part_button.dart';
 
 class ChosenProductCard extends StatelessWidget {
   final ChosenProduct chosenProduct;
 
   ChosenProductCard(this.chosenProduct);
 
+  static final _padding = const EdgeInsets.only(
+    top: 16.0,
+    left: 16.0,
+    right: 16.0,
+    bottom: 0.0,
+  );
+
   @override
   Widget build(BuildContext context) {
     final product = chosenProduct.product;
 
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0, bottom: 16.0),
-            child: ProductDescription(product),
-          ),
-        ],
+      child: Padding(
+        padding: _padding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ProductDescription(product),
+            Center(
+              child: TakePartButton(
+                onTap: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/screens/bill_details/components/take_part_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TakePartButton extends StatelessWidget {
+  final void Function() onTap;
+
+  TakePartButton({
+    @required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textStyle = theme.textTheme.bodyText1.copyWith(
+      color: theme.primaryColor,
+    );
+
+    return FlatButton(
+      onPressed: () {},
+      child: Text(
+        "THAT'S MINE",
+        style: textStyle,
+      ),
+    );
+  }
+}
+
