6fd44f76d2f07e5da60ed56400e97a69c04de2f8
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-05-16 00:17:45 +0300

    Refactor DebtCard

--- lib/src/screens/debts/components/debt_card.dart
+++ lib/src/screens/debts/components/debt_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splitbill_client/src/components/users/user_avatar.dart';
 import 'package:splitbill_client/src/models/debt.dart';
-import 'package:splitbill_client/src/screens/debts/components/accept_payment_action.dart';
 
 class DebtCard extends StatelessWidget {
   final Debt debt;
@@ -13,33 +12,52 @@ class DebtCard extends StatelessWidget {
     final money = debt.money;
     final recipient = debt.recipient;
 
+    final spacer = const SizedBox(width: 8);
+
     return Card(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: AspectRatio(
-                    aspectRatio: 2,
-                    child: UserAvatar(user: recipient, size: 60),
-                  ),
-                ),
-                Flexible(flex: 4, child: Text(recipient.name)),
-                Flexible(
-                  flex: 2,
-                  child: Text(
-                    "${money.toString()} $symbol",
-                    style: TextStyle(
-                      color: textColor,
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 4,
+              right: 4.0,
+            ),
+            child: Container(
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: UserAvatar(user: recipient, size: 60),
                     ),
                   ),
-                ),
-              ],
+                  spacer,
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      width: double.infinity,
+                      child: Text(recipient.name),
+                    ),
+                  ),
+                  spacer,
+                  Flexible(
+                    flex: 2,
+                    child: Text(
+                      "${money.toString()} $symbol",
+                      style: TextStyle(
+                        color: textColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
