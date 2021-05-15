f940d6b86ed6a0c09d5ea0239e5265f30f7f98dc
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-05-16 00:20:42 +0300

    Create debt info comppnent

--- lib/src/screens/debts/components/debt_card.dart
+++ lib/src/screens/debts/components/debt_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:splitbill_client/src/components/users/user_avatar.dart';
 import 'package:splitbill_client/src/models/debt.dart';
+import 'package:splitbill_client/src/screens/debts/components/debt_info.dart';
 
 class DebtCard extends StatelessWidget {
   final Debt debt;
@@ -9,11 +9,6 @@ class DebtCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final money = debt.money;
-    final recipient = debt.recipient;
-
-    final spacer = const SizedBox(width: 8);
-
     return Card(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -25,47 +20,10 @@ class DebtCard extends StatelessWidget {
               left: 4,
               right: 4.0,
             ),
-            child: Container(
-              height: 40,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: AspectRatio(
-                      aspectRatio: 1,
-                      child: UserAvatar(user: recipient, size: 60),
-                    ),
-                  ),
-                  spacer,
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      width: double.infinity,
-                      child: Text(recipient.name),
-                    ),
-                  ),
-                  spacer,
-                  Flexible(
-                    flex: 2,
-                    child: Text(
-                      "${money.toString()} $symbol",
-                      style: TextStyle(
-                        color: textColor,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            child: DebtInfo(debt),
           ),
         ],
       ),
     );
   }
-
-  bool get isPositive => debt.money > 0;
-  Color get textColor => isPositive ? Colors.green : Colors.red;
-  String get symbol => isPositive ? "📈" : "📉";
 }
---
+++ lib/src/screens/debts/components/debt_info.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:splitbill_client/src/models/debt.dart';
+import 'package:splitbill_client/src/components/users/user_avatar.dart';
+
+class DebtInfo extends StatelessWidget {
+  final Debt debt;
+
+  DebtInfo(this.debt);
+
+  bool get isPositive => debt.money > 0;
+  Color get textColor => isPositive ? Colors.green : Colors.red;
+  String get symbol => isPositive ? "📈" : "📉";
+
+  @override
+  Widget build(BuildContext context) {
+    final money = debt.money;
+    final recipient = debt.recipient;
+
+    final spacer = const SizedBox(width: 8);
+
+    return Container(
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 1,
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: UserAvatar(user: recipient, size: 60),
+            ),
+          ),
+          spacer,
+          Flexible(
+            flex: 3,
+            child: Container(
+              width: double.infinity,
+              child: Text(recipient.name),
+            ),
+          ),
+          spacer,
+          Flexible(
+            flex: 2,
+            child: Text(
+              "${money.toString()} $symbol",
+              style: TextStyle(
+                color: textColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
